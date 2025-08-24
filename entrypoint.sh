c4e50e3026defe70a505bd231cdcdf5a1158fdf3
xyouo <88232776+xyouo@users.noreply.github.com>
2025-08-24 22:11:20 +0800

    1

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-useradd -m -s /bin/bash $SSH_USER
-echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
-usermod -aG sudo $SSH_USER
-echo "$SSH_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/init-users
-echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
-
-exec "$@"
